9e4569167201056d5728ce5c8f389261fb5ee3d9
William <macedo.franca@gmail.com>
2019-12-04 13:44:46 -0300

    Lazyload modules, authguard and common module created

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,16 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import {LoginComponent} from './components/public/login/login.component';
+import {AuthguardService} from './services/authguard.service';
 
 const routes: Routes = [
-  { path: '', redirectTo: 'login', pathMatch: 'full' },
+  { path: '', redirectTo: 'admin/painel', pathMatch: 'full' },
   { path: 'login', component: LoginComponent },
+  {
+    path: 'admin/painel',
+    loadChildren: './components/admin/painel/painel.module#PainelModule',
+    canActivate: [AuthguardService]
+  },
 ];
 
 @NgModule({
